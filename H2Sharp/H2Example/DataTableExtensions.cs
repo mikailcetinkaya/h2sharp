925263a514d5cbcd2783c7d80127b8a0482b5725
Mikail Cetinkaya <mikail.cetinkaya@paycore.com>
2021-10-27 13:53:52 +0300

    ready to cook

---
+++ H2Sharp/H2Example/DataTableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2Example
+{
+    public static class DataTableExtensions
+    {
+        public static String ToXml(this DataTable table)
+        {
+            var o = new MemoryStream();
+            table.WriteXml(o);
+            var t = new StringWriter();
+            o.Close();
+            o = new MemoryStream(o.GetBuffer());
+            var reader = new StreamReader(o);
+            return reader.ReadToEnd();
+        }
+    }
+}
